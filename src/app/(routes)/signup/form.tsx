ed903c15e054efb8c5319e8042dc49d8a898b409
Kartikey Mishra <kartikeymishracsjm@gmail.com>
2025-02-10 12:35:58 +0530

    init

--- src/app/(routes)/signup/form.tsx
+++ src/app/(routes)/signup/form.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { SignUp } from './action'
 import { googleSignIn } from './action'
 const page = () => {
